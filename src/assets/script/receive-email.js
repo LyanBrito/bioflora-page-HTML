d84a7463d84707ddde633ba9a2b6d4c245b565e0
Lyan <lyanbrito613@gmail.com>
2024-09-16 17:45:32 -0300

    feat:added-better-features

---
+++ src/assets/script/receive-email.js
@@ -0,0 +1,21 @@
+import env from "../config/config.js";
+
+emailjs.init({
+  publicKey: env.jS_APP_EMAILJS_PUBLICKEY,
+});
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(env.jS_APP_EMAILJS_SERVICEID, env.jS_APP_EMAILJS_TEMPLATEID_RECEIVED, this)
+      .then(() => {
+        console.log("Success!");
+        alert("Email enviado com sucesso");
+      }), (err) => {
+        console.log("Fail", err);
+        alert("Falha ao enviar o email, tente mais tarde!");
+      };
+  });
